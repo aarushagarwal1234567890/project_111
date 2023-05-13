417024c107bbb528ed60aa3b58e2b960ec2cebc6
aarushagarwal1234567890 <94752004+aarushagarwal1234567890@users.noreply.github.com>
2023-05-13 19:05:43 +0530

    Add files via upload

---
+++ Move_file.py
@@ -0,0 +1,15 @@
+import os 
+import shutil
+
+from_dir ="C:/Users/Aarush Agarwal/Downloads"
+to_dir = "C:/Users/Aarush Agarwal/Desktop/Document_Files"
+
+
+list_of_files = os.listdir(from_dir)
+print(list_of_files)
+
+
+for file_name in list_of_files:
+   name, extension = os.path.splitext(file_name)
+   print(name)
+   print(extension)
